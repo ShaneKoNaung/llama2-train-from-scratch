7d1cf6c55ce659a3d0d81d92d1e5fb59a8c68e58
ShaneKoNaung <shanekonaung@gmail.com>
2024-03-04 02:55:27 +0630

    fix config in train_thai.py

--- train_thai.py
+++ train_thai.py
@@ -25,7 +25,7 @@ log_interval = 1
 eval_iters = 1000
 eval_only = False  # if True, script exits right after the first eval
 always_save_checkpoint = False  # if True, always save a checkpoint after each eval
-init_from = "resume"  # 'scratch' or 'resume'
+init_from = "scratch"  # 'scratch' or 'resume'
 # wandb logging
 wandb_log = False  # disabled by default
 wandb_project = "llamac"
